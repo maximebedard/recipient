6faedd11f0a2522a2b41f53bb296644536ac1054
Maxime Bedard <maxime.bedard@shopify.com>
2015-02-09 22:18:14 -0500

    Deleted useless scss, added recipe ingredient specs

--- test/models/recipe_ingredient_test.rb
+++ test/models/recipe_ingredient_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class RecipeIngredientTest < ActiveSupport::TestCase
   def setup
     @subject = FactoryGirl.create :recipe_ingredient
+    @new_subject = RecipeIngredient.new
   end
 
   test '#total_calories returns the adjusted calories for this portion' do
@@ -22,5 +23,25 @@ class RecipeIngredientTest < ActiveSupport::TestCase
 
   test '#respond_to is false for total_serving since it is not permitted' do
     refute_respond_to @subject, :total_serving
+  end
+
+  test '#quantity is present' do
+    assert_presence_of @new_subject, :quantity
+  end
+
+  test '#ingredient is present' do
+    assert_presence_of @new_subject, :ingredient
+  end
+
+  test '#recipe is present' do
+    assert_presence_of @new_subject, :recipe
+  end
+
+  test '#ingredient_serving_unit returns the associated unit of the ingredient' do
+    assert :gram, @subject.ingredient_serving_unit
+  end
+
+  test '#convert_quantity_to_match_ingredient returns the quantity with the matching unit' do
+    assert Unitwise(1000, 'g'), @subject.convert_quantity_to_match_ingredient
   end
 end
